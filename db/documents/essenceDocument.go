68980c50587394924c7510dba14ff1f425ce7e42
Dmitriy <maliciousgenius@gmail.com>
2017-07-17 19:26:14 +0300

    first

--- db/documents/essenceDocument.go
+++ db/documents/essenceDocument.go
@@ -1,8 +1,8 @@
 package documents
 
 type EssenceDocument struct {
-	Id              string `bson:"_id,omitempty"`
-	Title           string
-	ContentHtml     string
-	ContentMarkdown string
+	id      string `bson:"_id,omitempty"`
+	name    string
+	content string
+	time    string
 }
